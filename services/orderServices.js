1e0f05b71224ef98aafcf5902f0bfcacf84b4a12
Marfil, Marvie Yuki <marvie.yuki.marfil@accenture.com>
2021-08-06 17:39:38 +0800

    insert/convert

--- services/orderServices.js
+++ services/orderServices.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const models = require('../models');
+const pmlConvertionService = require('./pmlConvertionService');
 
 
 
@@ -15,12 +16,81 @@ const orderServices = {
             res.send(result);
         } catch (error) {
             console.log(error);
+            res.status(500).send("Something went wrong");
         }
         // return res.send('This is an API');
     },
 
-    postOrders(req,res) {
-        return res.send('Hi POST');
+    async postOrders(req,res) {
+        try {
+            var message = 'Something Went Wrong'
+            const requestFromClient = req.body.pml;
+            let xmlInput = pmlConvertionService.transformPmlToXml(requestFromClient)
+            let requestJson = pmlConvertionService.xmlToJSon(xmlInput)
+
+            // let pizzaLength = requestInJsonFormat.order.pizza.length;
+            requestJson.order['order_number'] = requestJson.order._attributes.number;
+            
+            delete requestJson.order._attributes;
+            let pizzaLength = requestJson.order.pizza.length;
+
+            if(pizzaLength > 24) {
+                message = 'You can only order a maximum of 24 pizza'
+                throw new Error();
+            }
+            console.log(pizzaLength)
+
+            for (let i = 0; i < pizzaLength; i++) {
+                requestJson.order.pizza[i]['pizza_number'] = requestJson.order.pizza[i]._attributes.number
+                delete requestJson.order.pizza[i]._attributes;
+                requestJson.order.pizza[i]['size'] = requestJson.order.pizza[i].size._text
+                requestJson.order.pizza[i]['crust'] = requestJson.order.pizza[i].crust._text
+                requestJson.order.pizza[i]['type'] = requestJson.order.pizza[i].type._text
+
+                if(requestJson.order.pizza[i]['type'] == 'custom') {
+                    let pizzaToppingsLength = requestJson.order.pizza[i].toppings.length;
+                    if(pizzaToppingsLength <= 0){
+                        message = 'Invalid format.'
+                        throw new Error();
+                    }
+                    console.log(pizzaToppingsLength)
+                    for (let j = 0; j < pizzaToppingsLength; j++) {
+                        requestJson.order.pizza[i].toppings[j]['toppings_area'] = requestJson.order.pizza[i].toppings[j]._attributes.area;
+                        delete requestJson.order.pizza[i].toppings[j]._attributes;
+                        let pizzaItemsLength = requestJson.order.pizza[i].toppings[j].item.length
+                        if(pizzaItemsLength > 12){
+                            message = 'You can only have a maximum of 12 toppings item.'
+                            throw new Error();
+                        }
+
+                        if (pizzaItemsLength !== undefined) {
+                            for (let k = 0; k < pizzaItemsLength; k++) {
+                                requestJson.order.pizza[i].toppings[j].item[k]['toppings_item'] = requestJson.order.pizza[i].toppings[j].item[k]._text;
+                                delete requestJson.order.pizza[i].toppings[j].item[k]._text;
+    
+                            }                            
+                        } else {
+                            requestJson.order.pizza[i].toppings[j].item['toppings_item'] = requestJson.order.pizza[i].toppings[j].item._text;
+                            delete requestJson.order.pizza[i].toppings[j].item._text;
+                        }
+                    }
+                }
+            }
+            console.log(requestJson.order)
+            // // console.log(requestJson.order.pizza.toppings.toppings_area)
+            // res.send(requestJson);
+            const result = await models.Order.create(requestJson.order,
+                {
+                    include: {
+                    model: models.Pizza,
+                    required: true,
+                    include: [ models.Pizza_Addons]
+                }});
+            res.send(result);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(message);
+        }
     }
 }
 
---
+++ services/pmlConvertionService.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const convert = require('xml-js');
+
+const pmlConvertionService = {
+    transformPmlToXml(data) {
+        console.log(data)
+        let changedLeftCurleyBrace = data.replaceAll('{', '<')
+        let changedOpeningClosingTag = changedLeftCurleyBrace.replaceAll('}', '>')
+        const pmlToXML = changedOpeningClosingTag.replaceAll(/\\/g, '/')
+
+        return pmlToXML
+    },
+
+    xmlToJSon(xml) {
+        var options = {
+            compact: true,
+            trim: true,
+            spaces: 4,
+            ignoreDeclaration: true,
+            ignoreInstruction: true,
+            ignoreComment: true,
+            ignoreCdata: true,
+            ignoreDoctype: true,
+        };
+        const result = convert.xml2json(xml, options);
+        return JSON.parse(result);
+    }
+
+}
+
+module.exports = pmlConvertionService;
